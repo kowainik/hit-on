29028d9f8c4dee5d0dfe2b398d1773a230bd2358
Dmitrii Kovanikov <kovanikov@gmail.com>
2019-02-05 16:04:57 +0800

    [#3] Implement 'fresh' command (#17)

--- src/Hit/Git.hs
+++ src/Hit/Git.hs
@@ -1,15 +1,26 @@
+{-# LANGUAGE ViewPatterns #-}
+
 -- | Logic for CLI commands to make GitHub workflows easier.
 
 module Hit.Git
        ( runHop
+       , runFresh
        ) where
 
 import Hit.Shell ()
 
 
--- | @hit hop@ command
+-- | @hit hop@ command.
 runHop :: Maybe Text -> IO ()
-runHop branchName = do
-    let branch = fromMaybe "master" branchName
+runHop (nameOrMaster -> branch) = do
     "git" ["checkout",  branch]
     "git" ["pull", "--rebase", "--prune"]
+
+-- | @hit fresh@ command.
+runFresh :: Maybe Text -> IO ()
+runFresh (nameOrMaster -> branch) = do
+    "git" ["fetch", "origin", branch]
+    "git" ["rebase", "origin/" <> branch]
+
+nameOrMaster :: Maybe Text -> Text
+nameOrMaster = fromMaybe "master"
