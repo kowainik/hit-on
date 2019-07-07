cc319e4df512479af1111851d2b8b6f1981767af
Kahlil Abreu <kahlil29@gmail.com>
2019-07-08 01:29:42 +0530

    Formatting adjustments and addition of non-happy test case scenarios
    - Remove LANGUAGE pragmas
    - Format and arrange imports, add explicit imports
    - Minor improvements

--- test/Main.hs
+++ test/Main.hs
@@ -1,28 +1,32 @@
-{-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 
-module Main where
+module Main (main) where
 
-import GitHub.Data.Name
+import GitHub.Data.Name (Name (..))
+import Test.Hspec (describe, hspec, it, shouldBe)
+
 import Hit.Issue (parseOwnerRepo)
-import Test.Hspec
 
 
 main :: IO ()
 main = hspec $ do
-  describe "parseOwnerRepo" $ do
-    let expectedOwnerName = N "kowainik"
-    let expectedRepoName = N "hit-on"
-    it "Parses a GitHub repo link (SSH) and gives use the Owner Name and Repo Name separately" $ do
-      parseOwnerRepo "git@github.com:kowainik/hit-on.git" `shouldBe` (Just (expectedOwnerName, expectedRepoName))
+    describe "parseOwnerRepo" $ do
+        let expectedOwnerName = N "kowainik"
+        let expectedRepoName = N "hit-on"
+        let expectedCredentials = Just (expectedOwnerName, expectedRepoName)
+        it "Parses a GitHub repo link (SSH) and gives use the Owner Name and Repo Name separately" $ do
+            parseOwnerRepo "git@github.com:kowainik/hit-on.git" `shouldBe` expectedCredentials
+        it "Parses a GitHub repo link (SSH) and gives use the Owner Name and Repo Name separately" $ do
+            parseOwnerRepo "git@github.com:kowainik/hit-on" `shouldBe` expectedCredentials
+        it "Parses a GitHub repo link (HTTPS) and gives use the Owner Name and Repo Name separately" $ do
+            parseOwnerRepo "https://github.com/kowainik/hit-on.git" `shouldBe` expectedCredentials
+        it "Parses a GitHub repo link (HTTPS) and gives use the Owner Name and Repo Name separately" $ do
+            parseOwnerRepo "https://github.com/kowainik/hit-on" `shouldBe` expectedCredentials
+        it "Parses an invalid GitHub repo link and returns a `Nothing`" $ do
+            parseOwnerRepo "github.com/kowainik/hit-on" `shouldBe` Nothing
+        it "Parses an invalid GitHub repo link and returns a `Nothing`" $ do
+            parseOwnerRepo "https://githu.com/kowainik/hit-on" `shouldBe` Nothing
+        it "Parses an invalid GitHub repo link and returns a `Nothing`" $ do
+            parseOwnerRepo "https://github.com/kowainik" `shouldBe` Nothing
+        it "Parses an invalid GitHub repo link and returns a `Nothing`" $ do
+            parseOwnerRepo "git@github.com" `shouldBe` Nothing
 
-    it "Parses a GitHub repo link (SSH) and gives use the Owner Name and Repo Name separately" $ do
-      parseOwnerRepo "git@github.com:kowainik/hit-on" `shouldBe` (Just (expectedOwnerName, expectedRepoName))
-
-
-    it "Parses a GitHub repo link (HTTPS) and gives use the Owner Name and Repo Name separately" $ do
-      parseOwnerRepo "https://github.com/kowainik/hit-on.git" `shouldBe` (Just (expectedOwnerName, expectedRepoName))
-
-
-    it "Parses a GitHub repo link (HTTPS) and gives use the Owner Name and Repo Name separately" $ do
-      parseOwnerRepo "https://github.com/kowainik/hit-on" `shouldBe` (Just (expectedOwnerName, expectedRepoName))
