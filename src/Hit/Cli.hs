0771f8591ef3f0ba15548eda68db8aa0a44b4289
Georgi Bojinov <georgi.bojinov@hotmail.com>
2019-10-02 13:38:50 +0300

    [#107] Add local flag to amend command (#114)
    * [#107] Add local flag to amend command
    
    * When the flag is passed the `amend` command does not force push
    
    * [#107] Apply comments
    
    * Add documentation for amend flag
    
    Co-Authored-By: Dmitrii Kovanikov <kovanikov@gmail.com>

--- src/Hit/Cli.hs
+++ src/Hit/Cli.hs
@@ -37,7 +37,7 @@ hit = execParser cliParser >>= \case
     Commit opts -> runCommit opts
     Uncommit -> runUncommit
     Fix message pushBool -> runFix message pushBool
-    Amend -> runAmend
+    Amend localAmend -> runAmend localAmend
     Resolve branchName -> runResolve branchName
     Push isForce -> runPush isForce
     Sync -> runSync
@@ -70,6 +70,7 @@ data HitCommand
         (Maybe Text)  -- ^ Text of the fix commit
         PushBool      -- ^ Force push
     | Amend
+        Bool  -- ^ Local amend
     | Resolve (Maybe Text)
     | Push PushBool
     | Sync
@@ -153,7 +154,12 @@ fixP = do
     pure $ Fix commitMsg isForce
 
 amendP :: Parser HitCommand
-amendP = pure Amend
+amendP = do
+    localAmend <- switch
+        $ long "local"
+       <> short 'l'
+       <> help "Whether to do a local amend only - without pushing"
+    pure $ Amend localAmend
 
 pushP :: Parser HitCommand
 pushP = Push <$> pushBoolP
