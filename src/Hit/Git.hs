5b79274efe9eda6c2ab86c820d0cd64175ee3c9f
Max Strübing <mxstrbng@gmail.com>
2019-12-18 11:56:00 +0100

    Extract all internal helpers into `Common` module

--- src/Hit/Git.hs
+++ src/Hit/Git.hs
@@ -25,10 +25,8 @@ module Hit.Git
        , getUsername
        ) where
 
-import Control.Exception (bracket)
 import Data.Char (isAlphaNum, isDigit, isSpace)
 import GitHub (Issue (issueNumber), IssueNumber (..), unIssueNumber)
-import Shellmet (($|))
 import System.Directory (findExecutable)
 import System.Process (callCommand)
 
@@ -40,11 +38,19 @@ import Hit.Issue (createIssue, getIssueTitle, mkIssueId)
 
 import qualified Data.Text as T
 
-import Hit.Git.Common (nameOrMaster)
 import Hit.Git.Fresh (runFresh)
 import Hit.Git.Hop (runHop)
 import Hit.Git.Log (runLog)
 import Hit.Git.Uncommit (runUncommit)
+
+import Hit.Git.Common 
+    ( nameOrMaster
+    , getUsername
+    , getCurrentBranch
+    , issueFromBranch
+    , withDeletedFiles
+    , withUntrackedFiles
+    )
 
 -- QUESTION: should we somehow move this into separate module or split this module
 --           smaller parts?
@@ -269,72 +275,3 @@ runClone txt = do
             exitFailure
     let gitLink = "git@github.com:" <> name <> ".git"
     "git" ["clone", gitLink]
-
-----------------------------------------------------------------------------
--- Internal helpers
-----------------------------------------------------------------------------
-
--- | Get current user name from the local global git config.
-getUsername :: IO Text
-getUsername = do
-    login <- "git" $| ["config", "user.login"]
-    if login == ""
-        then errorMessage "user.login is not specified" >> exitFailure
-        else pure login
-
--- | Get the name of the current branch.
-getCurrentBranch :: IO Text
-getCurrentBranch = "git" $| ["rev-parse", "--abbrev-ref", "HEAD"]
-
-{- | Extracts issue number from the branch in form like:
-
-@
-kowainik/<n>-short-description
-@
--}
-issueFromBranch :: Text -> Maybe Int
-issueFromBranch =
-      readMaybe
-    . toString
-    . T.takeWhile isDigit
-    . T.drop 1
-    . T.dropWhile (/= '/')
-
-{- | Perform the given action by first staging the given files and
-later removing them again after the action
--}
-withFiles :: IO [Text] -> IO a -> IO a
-withFiles whichFiles action = bracket
-    addFiles
-    removeFiles
-    (const action)
-  where
-    addFiles :: IO [Text]
-    addFiles = do
-        files <- whichFiles
-        for_ files $ \file -> void $ "git" $| ["add", file]
-        pure files
-
-    -- Return files back to not spoil git state and have unexpected behavior
-    removeFiles :: [Text] -> IO ()
-    removeFiles = mapM_ $ \file -> void $ "git" $| ["reset", "--", file]
-
-{- | Perform given action by adding all deleted files to index and returning
-them back after action.
--}
-withDeletedFiles :: IO a -> IO a
-withDeletedFiles = withFiles deletedFiles
-  where
-    -- Find the deleted file to index so they will appear in diff
-    deletedFiles :: IO [Text]
-    deletedFiles = lines <$> "git" $| ["ls-files", "--deleted", "--exclude-standard"]
-
-{- | Perform given action by adding all untracked files to index and returning
-them back after action.
--}
-withUntrackedFiles :: IO a -> IO a
-withUntrackedFiles = withFiles untrackedFiles
-  where
-    -- Find the untracked file to index so they will appear in diff
-    untrackedFiles :: IO [Text]
-    untrackedFiles = lines <$> "git" $| ["ls-files", "--others", "--exclude-standard"]
